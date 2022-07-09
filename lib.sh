b063e8da69fb030575d40e8d590cd5f2ec44f299
/:) <g.yevgeniy.p@gmail.com>
2022-07-09 15:03:47 +0200

    + systemctl checking

--- lib.sh
+++ lib.sh
@@ -235,21 +235,35 @@ chk_SvsStatus() {
 # Checking active status from systemd unit (prev)
 chk_SvcExist() {
     local n=$1
-    if [[ $(systemctl list-units --all -t service --full --no-legend "$n.service" | cut -f1 -d' ') == $n.service ]]; then
-        return 0
+
+    if ! type "systemctl" > /dev/null; then
+      Error "Error" "Systemctl does not found"
+      return 0
     else
-        return 1
+        if [[ $(systemctl list-units --all -t service --full --no-legend "$n.service" | cut -f1 -d' ') == $n.service ]]; then
+            return 0
+        else
+            return 1
+        fi
     fi
+    
 }
 
 # Checking active status from systemd unit (latest)
 service_exists() {
     local n=$1
-    if [[ $(systemctl list-units --all -t service --full --no-legend "$n.service" | sed 's/^\s*//g' | cut -f1 -d' ') == $n.service ]]; then
-        return 0
+
+    if ! type "systemctl" > /dev/null; then
+      Error "Error" "Systemctl does not found"
+      return 0
     else
-        return 1
+        if [[ $(systemctl list-units --all -t service --full --no-legend "$n.service" | sed 's/^\s*//g' | cut -f1 -d' ') == $n.service ]]; then
+            return 0
+        else
+            return 1
+        fi
     fi
+    
 }
 
 
